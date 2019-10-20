b32b0706b957ecbcd3a6b1f6fc2d766116e1c883
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2019-10-20 18:32:48 -0300

    Adicionar arquivos de projeto.

---
+++ tp1/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace tp1.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ tp1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="tp1.WebApiApplication" Language="C#" %>
